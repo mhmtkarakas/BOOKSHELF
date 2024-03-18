becc9718b5197e5ba4b5073b2dab697e1a43aa8f
mhmtkarakas <m.karakas32@gmail.com>
2024-03-18 15:50:44 +0300

    ListCategories

--- src/pages/ListCategories.jsx
+++ src/pages/ListCategories.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+import CustomModal from "./../components/CustomModal";
+
+import api from "../api/api";
+import urls from "../api/urls";
+
 const ListCategories = () => {
+  // Modal icin state tutuyoruz
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+ // Belirledigimiz kategoriyi silmek icin state tutariz
+  const [willDeleteCategory,setWillDeleteCategory] = useState("");
   // storumuza abone oluyoruz
   const { categoryState, booksState } = useSelector((state) => state);
+
+  const deleteCategory = (id) =>{
+    // silmek istedigimiz kategoriye ait kitaplari bulmamiz gerekir.
+    const book = booksState.books.filter(item=>item.categoryId === id)
+    console.table(book);
+    api.delete(`${urls.categories}/${id}`) //veri tabanindan sileriz
+    .then((resCat)=>{
+        // Kategory silme islemini dispatch ederiz
+        booksState.map(item =>{
+          api.delete(`${urls.books}/${item.id}`)
+          .then((resBook)=>{
+            // dispatch delete book
+          })
+          .catch((err)=>{})
+        })
+    })
+    .catch((err)=>{})
+  }
 
   return (
     <div className="container my-5">
@@ -42,7 +69,17 @@ const ListCategories = () => {
                     <td>{category.name}</td>
                     <td>{books.length}</td>
                     <td>
-                      <button className="btn btn-sm btn-danger">Sil</button>
+                      {/* Kategory sildigimiz zaman o kategoriye ait butun kitaplari da silmemiz gerekir.
+                      Bu islemi hem merkezi storumuzda hem de veri tabaninda gerceklestirmemiz gerekir */}
+                      <button
+                        onClick={() => {
+                          setOpenDeleteModal(true);
+                          setWillDeleteCategory(category.id) // silmek istedigimiz kategory
+                        }}
+                        className="btn btn-sm btn-danger"
+                      >
+                        Sil
+                      </button>
                       <Link
                         className="btn btn-sm btn-secondary"
                         to={`/edit-category/${category.id}`}
@@ -57,6 +94,19 @@ const ListCategories = () => {
           )}
         </tbody>
       </table>
+      {
+        // Modelimizda olmasini istedigimiz mesajlari prop olarak aliyoruz.
+        openDeleteModal === true && (
+          <CustomModal
+            title="Kategory Silme"
+            mesaj="Kategori ile beraber ilgili butun kitaplar da silinecektir. Bu islemi yapmak istediginize emin misiniz?"
+            // Kullanici silme islemini iptal edebilir. Bunun icin fonksiyon propunu import ederiz.
+            onCancel = {()=>{setOpenDeleteModal(false)}}
+
+            onConfirm = {()=>deleteCategory(willDeleteCategory)} // Bu islemle kategory silinecek. Yukarda fonksiyonumuzu calistiracagiz.
+          />
+        )
+      }
     </div>
   );
 };
